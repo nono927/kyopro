d7d51c2c7d52c0e415a774e2945a32733ca4aa29
Naoki <poteto9270@g.ecc.u-tokyo.ac.jp>
2021-03-02 23:47:52 +0900

    modified test

--- mylib/test/test_cht.cpp
+++ mylib/test/test_cht.cpp
@@ -1,5 +1,58 @@
 #include "gtest/gtest.h"
 #include "cht.hpp"
+#include <vector>
+#include <map>
+
+template<typename T>
+class ConvexHullTrickNaive {
+    std::map<T, T> lines;
+
+public:
+    ConvexHullTrickNaive() {}
+
+    void add(T a, T b) {
+        auto itr = lines.find(a);
+        if (itr == lines.end() || b < itr->second) {
+            lines[a] = b;
+        }
+    }
+
+    T get(T x) {
+        T ret = std::numeric_limits<T>::max();
+        for (auto &&p : lines) {
+            ret = std::min(ret, p.first * x + p.second);
+        }
+        return ret;
+    }
+};
+
+class ConvexHullTrickTest : public ::testing::Test {
+protected:
+    ConvexHullTrick<int> cht;
+    ConvexHullTrickNaive<int> cht_naive;
+
+    void add(int a, int b) {
+        cht.add(a, b);
+        cht_naive.add(a, b);
+    }
+
+    bool ok(int x) {
+        return cht.get(x) == cht_naive.get(x);
+    }
+};
+
+template<typename T>
+struct Query {
+    enum class Type {
+        ADD,
+        GET,
+    };
+    Type type;
+    T a, b, x;
+
+    Query(T a, T b) : type(Type::ADD), a(a), b(b) {}
+    Query(T x) : type(Type::GET), x(x) {}
+};
 
 TEST(ConvexHullTrick, Single) {
     ConvexHullTrick<int> cht;
@@ -18,4 +71,27 @@ TEST(ConvexHullTrick, Small) {
     ASSERT_EQ(cht.get(-1), -2);
     cht.add(-1, 0);
     ASSERT_EQ(cht.get(-1), -2);
+}
+
+TEST_F(ConvexHullTrickTest, Monotonic) {
+    std::vector<Query<int> > queries = {
+        Query(5, 3),
+        Query(3, 2),
+        Query(-1),
+        Query(2, 0),
+        Query(0, 6),
+        Query(-1, 4),
+        Query(1),
+        Query(-2, 7),
+        Query(-4, 1),
+        Query(2),
+    };
+
+    for (auto &&q : queries) {
+        if (q.type == Query<int>::Type::ADD) {
+            add(q.a, q.b);
+        } else {
+            ASSERT_TRUE(ok(q.x));
+        }
+    }
 }
