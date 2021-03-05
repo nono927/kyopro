ab316b25db98ef6b4a2a7e2f7cc25dcd37bd6ab2
Naoki <poteto9270@g.ecc.u-tokyo.ac.jp>
2021-03-05 21:39:07 +0900

    added testcase generator

---
+++ marathon/tsp/gen_testcase.py
@@ -0,0 +1,35 @@
+import os
+import argparse
+import numpy as np
+
+def gen_tsp_testcase(filename, n, h, w=None):
+    if w is None:
+        w = h
+    assert(h > 0)
+    assert(w > 0)
+    assert(n <= h * w)
+
+    points = np.arange(h * w)
+    np.random.shuffle(points)
+
+    with open(filename, "w") as f:
+        f.write("%d\n" % n)
+        for p in points[0:n]:
+            f.write("%d %d\n" % (p // w, p % w))
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--num_cases", type=int, default=1)
+    parser.add_argument("--dirname", type=str, default="testcase")
+    parser.add_argument("--seed", type=int, default=1)
+    args = parser.parse_args()
+
+    np.random.seed(seed=args.seed)
+    if not os.path.isdir(args.dirname):
+        os.makedirs(args.dirname)
+
+    for i in range(args.num_cases):
+        fname = os.path.join(args.dirname, "case{}.txt".format(i))
+        gen_tsp_testcase(fname, 10, 100)
+
