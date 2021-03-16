eaff3531566505c724de082e3b5d702180b4d657
nono927 <n_ono@hal.t.u-tokyo.ac.jp>
2021-03-16 13:07:33 +0000

    added setup.py

---
+++ marathon/tsp/setup.py
@@ -0,0 +1,30 @@
+import os
+import yaml
+import argparse
+import numpy as np
+
+from scripts.gen_testcase import gen_tsp_testcase
+
+testcase_root_dir = "sample"
+
+def gen_testcases(testname, num_nodes, num_cases, h, w):
+    dirname = os.path.join(testcase_root_dir, testname)
+    if not os.path.isdir(dirname):
+        os.makedirs(dirname)
+    for i in range(num_cases):
+        fname = os.path.join(dirname, "case{}.txt".format(i))
+        gen_tsp_testcase(fname, num_nodes, h, w)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--seed", type=int, default=1)
+    args = parser.parse_args()
+
+    np.random.seed(seed=args.seed)
+
+    with open("config.yml", "r") as f:
+        config = yaml.safe_load(f)
+
+    for testname in config["testcases"]:
+        cfg = config["testcases"][testname]
+        gen_testcases(testname, cfg["num_nodes"], cfg["num_cases"], cfg["height"], cfg["width"])
