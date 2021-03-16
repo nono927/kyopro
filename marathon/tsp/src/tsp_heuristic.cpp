59e54f7abec6cd24a9d5bcc512d8f1964b49523e
nono927 <n_ono@hal.t.u-tokyo.ac.jp>
2021-03-16 14:49:16 +0000

    implemented greedy algo

--- marathon/tsp/src/tsp_heuristic.cpp
+++ marathon/tsp/src/tsp_heuristic.cpp
@@ -1,16 +1,43 @@
-#include <iostream>
-#include <fstream>
 #include <vector>
-#include <string>
 #include <cmath>
 #include <limits>
-#include <cassert>
+
+#include <dice.hpp>
+#include <timer.hpp>
 
 using namespace std;
+const auto INF = numeric_limits<int>::max();
+
+int dist(int x0, int x1, int y0, int y1) {
+    return abs(x0 - x1) + abs(y0 - y1);
+}
+
+int nearest_neighbor(int tmp, vector<bool> &used, vector<int> &xs, vector<int> &ys) {
+    const int n = used.size();
+    int mind = INF;
+    int ret = -1;
+    for (int i = 0; i < n; ++i) {
+        if (used[i]) continue;
+        auto d = dist(xs[tmp], xs[i], ys[tmp], ys[i]);
+        if (d < mind) {
+            mind = d;
+            ret = i;
+        }
+    }
+    return ret;
+}
 
 vector<int> tsp_heuristic(vector<int> &xs, vector<int> &ys) {
     const int n = xs.size();
     vector<int> ret(n);
-    for (int i = 0; i < n; ++i) ret[i] = i;
+    vector<bool> used(n);
+
+    // greedy
+    for (int i = 1, tmp = 0; i < n; ++i) {
+        used[tmp] = true;
+        tmp = nearest_neighbor(tmp, used, xs, ys);
+        ret[i] = tmp;
+    }
+
     return ret;
 }
