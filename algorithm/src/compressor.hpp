a89f0828e517683c37dd78d43d5235657482c7c3
nono927 <n_ono@hal.t.u-tokyo.ac.jp>
2021-05-12 10:56:20 +0000

    implemented

--- algorithm/src/compressor.hpp
+++ algorithm/src/compressor.hpp
@@ -1,15 +1,30 @@
 #include <vector>
+#include <algorithm>
 
 template <class T>
 class Compressor {
+    std::vector<T> vals;
+
 public:
-    Compressor(std::vector<T> &v){}
+    Compressor(std::vector<T> &v){
+        vals.resize(v.size());
+        std::copy(v.begin(), v.end(), vals.begin());
+        std::sort(vals.begin(), vals.end());
+        vals.erase(std::unique(vals.begin(), vals.end()), vals.end());
+    }
 
     std::vector<size_t> compress(std::vector<T> &v) {
-        return std::vector<size_t>();
+        std::vector<size_t> ret(v.size());
+        for (size_t i = 0; i < v.size(); ++i) {
+            ret[i] = std::distance(
+                        vals.begin(),
+                        std::lower_bound(vals.begin(), vals.end(), v[i])
+                    );
+        }
+        return ret;
     }
 
     T operator[](size_t i) {
-        return T();
+        return vals[i];
     }
 };
